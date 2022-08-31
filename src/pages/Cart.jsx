6a0466866e97569ed8bc957cfcdf92d3c5cc4faf
Gajraj Shekhawat <gaju2gajraj@gmail.com>
2022-08-31 14:37:24 +0530

    Css fixed

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "../components/Loader";
 import Product from "../components/Product";
-import { deleteFromCart } from "../redux/cart/actions";
+import { deleteFromCart, fetchCartData } from "../redux/cart/actions";
 import styles from "../styles/cart.module.css";
 
 const Cart = () => {
@@ -13,7 +14,9 @@ const Cart = () => {
   const DeleteProduct = (el) => {
     dispatch(deleteFromCart(el.id));
   };
-
+  useEffect(()=>{
+    dispatch(fetchCartData());
+  },[])
   if (isLoading) {
     return <Loader />;
   }
